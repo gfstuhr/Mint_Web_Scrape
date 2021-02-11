b9597fba889402ed142b0c69987aff89d1cf867d
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2021-02-11 12:02:22 -0700

    Trying to get email to send

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,33 @@
 from scrape import scrape
+from config import email_list
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
 
-a,b = scrape()
-print(a)
-print(b)
+all_budgets,transactions = scrape()
+
+recipients = [email_list[0]] 
+emaillist = [elem.strip().split(',') for elem in recipients]
+msg = MIMEMultipart()
+msg['Subject'] = "Daily Budget Update"
+msg['From'] = email_list[1]
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    {0}
+  </body>
+</html>
+""".format(all_budgets.to_html())
+
+part1 = MIMEText(html, 'html')
+msg.attach(part1)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.sendmail(msg['From'], emaillist , msg.as_string())
+server.quit()
