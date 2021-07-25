71ce25783eb36e188e0d8a94c3b5b00f96c1071f
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2021-07-25 11:23:34 -0600

    Scheduler

--- Code/main.py
+++ Code/main.py
@@ -6,15 +6,16 @@ from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP
 import smtplib, ssl
 import sys
-
-all_budgets,transactions = scrape()
-
-# Defining subjectline and sender email
-msg = MIMEMultipart()
-msg['Subject'] = 'Daily Budget Update'
-msg['From'] = gmail
+import schedule
+import time
 
 def send_email():
+  all_budgets,transactions = scrape()
+
+  # Defining subjectline and sender email
+  msg = MIMEMultipart()
+  msg['Subject'] = 'Daily Budget Update'
+  msg['From'] = gmail
   # Creating tables from Pandas DFs
   html = """\
   <html>
@@ -46,4 +47,8 @@ def send_email():
       server.login(gmail,gmail_pass)
       server.sendmail(gmail, email_list, msg.as_string()) 
 
-send_email()
+schedule.every().day.at("01:30").do(send_email)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
