041feecdbb58c3863cb917895505d4df9a4050e2
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2022-05-30 09:45:35 -0600

    Updated code for updated Mint website

--- Code/scrape.py
+++ Code/scrape.py
@@ -41,11 +41,12 @@ def scrape ():
     html=browser.html
     soup=BeautifulSoup(html,"html.parser")
 
-    # Find Income Data
-    income = soup.find('ul',id='incomeBudget-list-body').find_all('strong')
-    income_title = income[0].text
-    income_progress = income[1].text
-    income_goal = income[2].text
+    income = soup.find('div',{'class': 'BudgetCardstyle__WrapperBudget-k3uxfw-1 irMFKm'})
+    income_list = income.text.split('$')
+    income_title = income_list[0]
+    income_list = [re.sub("[^0-9]", "", s) for s in income_list]
+    income_progress = income_list[2]
+    income_goal = income_list[3]
     income_details = {
         'Name': income_title,
         'Budget Amount': income_progress,
@@ -53,28 +54,31 @@ def scrape ():
     }
 
     # Find Budget Data
-    budgets = soup.find('ul', id='spendingBudget-list-body')
-    budget_list =[]
-    for result in budgets.find_all('li',id=re.compile('budget-')):
-        budget_items = result.find_all('strong')
-        budget_name = budget_items[0].text
-        budget_spent = budget_items[1].text
-        budget_amount = budget_items[2].text
+    budgets = soup.find_all('div',{'class':'BudgetListstyle__BudgetListSection-sc-1oeq37j-8 fzKgiV'})[1]
+    budget_items =[]
+    for result in budgets.find_all('div',{'class': 'BudgetCardstyle__WrapperBudget-k3uxfw-1 irMFKm'}):
+        budget_list = result.text.split('$')
+        budget_name = budget_list[0]
+        budget_list = [re.sub("[^0-9]", "", s) for s in budget_list]
+        budget_spent = budget_list[-1]
+        budget_amount = budget_list[-2]
         budget_details = {
             'Name': budget_name,
             'Budget Amount': budget_amount,
             'Amount Spent': budget_spent
         }
-        budget_list.append(budget_details)
+        budget_items.append(budget_details)
 
-    #Find Everything Else Data
-    everything_else = soup.find('li',id='spendingEE-list-total').find_all('strong')
-    everything_title = everything_else[0].text
-    everything_spent = everything_else[2].text
-    everything_goal = everything_else[1].text
+    # #Find Everything Else Data
+    everything_else = soup.find_all('div',{'class': 'EverythingElsestyle__EverythingElseListSection-n6fqz4-0 fAfoAa'})[1]
+    everything_list = everything_else.text.split('$')
+    everything_title = everything_list[0]
+    everything_list = [re.sub("[^0-9]", "", s) for s in everything_list]
+    everything_progress = income_list[2]
+    everything_goal = income_list[3]
     everything_details = {
         'Name': everything_title,
-        'Budget Amount': everything_spent,
+        'Budget Amount': everything_progress,
         'Amount Spent': everything_goal
     }
 
@@ -83,14 +87,15 @@ def scrape ():
 
     # Converting dictonaries to dataframes
     income_df = pd.DataFrame([income_details])
-    budget_df = pd.DataFrame(budget_list)
+    budget_df = pd.DataFrame(budget_items)
     everything_df = pd.DataFrame([everything_details])
 
     # Combining all data frames
     all_budgets = pd.concat([income_df,budget_df,everything_df], ignore_index=True)
-    all_budgets[all_budgets.columns[1:]] = all_budgets[all_budgets.columns[1:]].replace('[\$,]', '', regex=True).astype(int)
+    all_budgets[all_budgets.columns[1:]] = all_budgets[all_budgets.columns[1:]].astype(int)
     all_budgets['Remaining Budget'] = all_budgets['Budget Amount'] - all_budgets['Amount Spent']
-
+    all_budgets
+    
     #return functions
     return all_budgets, transactions
 
